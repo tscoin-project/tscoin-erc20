014e64645da52156c4ee1c2d0b6eb3e7713dac3b
ekobagas_k <ekobagas.k@gmail.com>
2018-07-14 16:28:52 +0700

    3. Token Sale

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 var TSCoin = artifacts.require("./TSCoin.sol");
+var TSCoinSale = artifacts.require("./TSCoinSale.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(TSCoin, 200000000);
+  deployer.deploy(TSCoin, 200000000).then(function(){
+    var tokenPrice =  505648089155871.00000000000000000000; 
+    return deployer.deploy(TSCoinSale, TSCoin.address, tokenPrice);
+  });
+  
 };
